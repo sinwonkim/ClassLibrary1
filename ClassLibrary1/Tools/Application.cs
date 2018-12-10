6c7f5e77cc9f5dff45ad5b191eca3a803e0ed7e7
sinwonkim <zhfps114@gmail.com>
2018-12-10 17:20:39 +0900

    클래스 라이브러리 #1

---
+++ ClassLibrary1/Tools/Application.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Tools
+{
+    public static class Application
+    {
+        public static string Root
+        {
+            get
+            {
+                return AppDomain.CurrentDomain.BaseDirectory; 
+                //Application클래스에서  Root 프로퍼티를 호출하면 폴더를 리턴해주면되지
+
+            }
+        }
+    }
+}
---
+++ ClassLibrary1Testconsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1.Tools; //클래스 라이브러리의 툴 포함 시킴 
+
+namespace ClassLibrary1Testconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ClassLibrary1.Tools.Application.Root);
+            Console.ReadLine(); //컴파일 시키면 ClassLibray1이 있는 위치 리턴되서 cmd 창으로 보여지지 
+        }
+    }
+}

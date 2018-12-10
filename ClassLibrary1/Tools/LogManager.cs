6433293554970c597636a07ceda1b87ffe127a0a
sinwonkim <zhfps114@gmail.com>
2018-12-10 19:39:19 +0900

    # 로그 관련 라이브러리 만들기

---
+++ ClassLibrary1/Tools/LogManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO; // 네임스페이스 IO쪽 추가 시켜놈 
+
+namespace ClassLibrary1.Tools
+{
+    public class LogManager // 클래스는 퍼블릭으로 해놔야 하고 
+    {
+        private String _path;
+
+        #region Constructors
+        public LogManager(String path)
+        {
+            _path = path;
+            _SetLogPath();
+        }
+
+        public LogManager() //로그 매니저 호출
+            : this(Path.Combine(Application.Root, "Log"))
+        {
+        }
+        #endregion
+
+        #region Methods
+        private void _SetLogPath() // 
+        {
+            if (Directory.Exists(_path))  //파일 없으면 생성해라 
+                Directory.CreateDirectory(_path);
+
+            string logFile = DateTime.Now.ToString("yyyyMMdd" ) +".txt";
+            _path = Path.Combine(_path, logFile);
+
+        }
+
+        
+        public void Write(String data)
+        {
+            try
+            {
+
+            
+                // using 선언해서 하면 using 안에서만 작동하고 영역 벗어나면 resource 자동으로 해제 해줌 
+                using (StreamWriter writer = new StreamWriter(_path, true))
+                {
+                    writer.Write(data);
+                }
+            }
+            catch(Exception ex)
+            {
+          
+            }
+        }
+        
+        public void WriteLine(String data)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(_path, true))
+                {
+                    writer.WriteLine(DateTime.Now.ToString("yyyyMMdd HH:mm:ss/t") + data);
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+
+        }
+        #endregion
+    }
+}
--- ClassLibrary1Testconsole/Program.cs
+++ ClassLibrary1Testconsole/Program.cs
@@ -5,14 +5,28 @@ using System.Text;
 using System.Threading.Tasks;
 using ClassLibrary1.Tools; //클래스 라이브러리의 툴 포함 시킴 
 
+
 namespace ClassLibrary1Testconsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ClassLibrary1.Tools.Application.Root);
-            Console.ReadLine(); //컴파일 시키면 ClassLibray1이 있는 위치 리턴되서 cmd 창으로 보여지지 
+            LogManager log = new LogManager();
+
+            log.WriteLine("-----------[Begin Processing]--------");
+
+            for(int index = 0; index < 10; index++)
+            {
+                log.WriteLine("Processing : " + index);
+
+                System.Threading.Thread.Sleep(500);
+
+                log.WriteLine("Done : " + index);
+
+            }
+
+            log.WriteLine("--------[End Processing ]-------------");
         }
     }
 }
